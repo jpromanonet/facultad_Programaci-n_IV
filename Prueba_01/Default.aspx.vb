3c06688d09e67598c7fb0b7d55b1e0d9ab0c6bed
jpromanonet <programming@jpromano.net>
2020-09-06 19:15:39 -0600

    Clase 01

--- Prueba_01/Default.aspx.vb
+++ Prueba_01/Default.aspx.vb
@@ -2,7 +2,9 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Label1.Text = 2 * 5 - 6 / (1 + 1) + 5 * (4 - 1)
     End Sub
 
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Label1.Text = TextBox1.Text
+    End Sub
 End Class
