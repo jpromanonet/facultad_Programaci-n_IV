f4160ebdc8efdafcd450e6c35d2e952ca816fb8a
jpromanonet <programming@jpromano.net>
2020-09-13 17:57:08 -0600

    Clase 02

---
+++ Clase_02/Prueba_01/Default.aspx.vb
@@ -0,0 +1,10 @@
+﻿Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    End Sub
+
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Label1.Text = TextBox1.Text
+    End Sub
+End Class
