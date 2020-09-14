18f7a72086a95d6984e611d0eb0517ff70a500d0
jpromanonet <programming@jpromano.net>
2020-09-13 18:24:28 -0600

    Clase 02

--- Clase_02/Prueba_01/Default.aspx.vb
+++ Clase_02/Prueba_01/Default.aspx.vb
@@ -8,4 +8,8 @@
     Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
         Label1.Text = TextBox1.Text
     End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Label2.Text = DateTime.Now.ToString("hh:mm:ss") & "Esta hora es del Page Load"
+    End Sub
 End Class
